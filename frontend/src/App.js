492e283e096a6d5d1a5127b5296d569731d061fa
Matheus Reis Cunha <matheus.cunha@embrapii.org.br>
2025-06-11 10:33:04 -0300

    ajustes

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sistema Jurídico</h1>
+        <p>Bem-vindo ao sistema de gestão jurídica</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
